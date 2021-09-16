1bb167bd531dce76883ada7171e078ad05bb5553
Azhar Faturahman <13519020@std.stei.itb.ac.id>
2021-09-16 17:13:21 +0700

    fixing device models

--- Backend/src/models/Device.ts
+++ Backend/src/models/Device.ts
@@ -7,39 +7,49 @@ class Device extends Generic {
   // The id of device that used by the user.
   deviceId!: string;
 
-  roomTemp!: number;
+  roomTemp: number;
 
-  roomRH!: number;
+  roomRH: number;
 
-  userTemp!: number;
+  userTemp: number;
 
-  userSpO2!: number;
+  userSpO2: number;
 
-  userBPM!: number;
+  userBPM: number;
+
+  /**
+  * Type check that will validate if the input is a valid new user.
+  */
+  public static isDevice = (input: any): input is Device => {
+    try {
+      // Validate that each input properties are in the schema.
+      const validProperties = ['deviceId', 'roomTemp', 'roomRH', 'userTemp', 'userSpO2', 'userBPM'];
+      input.forEach((property: string) => {
+        if (!validProperties.includes(property)) {
+          throw new Error(`Invalid property: ${property}`);
+        }
+      });
+
+      // Validate the type of input
+      if (!input.deviceId || typeof input.deviceId !== 'string') return false;
+      if (input.deviceId && typeof input.roomTemp !== 'number') return false;
+      if (input.deviceId && typeof input.roomRH !== 'number') return false;
+      if (input.deviceId && typeof input.userTemp !== 'number') return false;
+      if (input.deviceId && typeof input.userSpO2 !== 'number') return false;
+      if (input.deviceId && typeof input.userBPM !== 'number') return false;
+
+      return true;
+    } catch (err) {
+      return false;
+    }
+  };
 
   // Set default table name.
   static get tableName() {
-    return 'user';
+    return 'device';
   }
 }
 
-/**
- * Type check that will validate if the input is a valid new user.
- */
-export const isDevice = (input: any): input is Device => {
-  try {
-    // Validate the type of input
-    if (typeof input.deviceId !== 'string') return false;
-    if (typeof input.roomTemp !== 'number') return false;
-    if (typeof input.roomRH !== 'number') return false;
-    if (typeof input.userTemp !== 'number') return false;
-    if (typeof input.userSpO2 !== 'number') return false;
-    if (typeof input.userBPM !== 'number') return false;
 
-    return true;
-  } catch (err) {
-    return false;
-  }
-};
 
 export default Device;
