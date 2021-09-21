3dcbb8fddd360b55eacb1bba36f99ca381940d07
Azhar Faturahman <13519020@std.stei.itb.ac.id>
2021-09-21 11:06:41 +0700

    testing

--- Backend/src/handlers/device.ts
+++ Backend/src/handlers/device.ts
@@ -134,6 +134,8 @@ export const updateDeviceById = async (req: Request, res: Response) => {
     // Get the params from the request.
     const { deviceId } = req.params;
 
+    console.log(req.body);
+
     // Validate the input.
     const errors = new ErrorContainer();
 
